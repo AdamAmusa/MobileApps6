cb853178c2da12aa2c6aae85e247fed4689b9e24
unknown <Adamamusa2004@gmail.com>
2023-03-09 15:54:13 +0000

    Lab 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{StudentService} from './Services/student.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'student-app';
+  students:any[]=[];
+  weather:any[]=[];
+
+  ngOnInit(): void{
+    this.service.getStudentData().subscribe((data) => {
+      this.students = data.students;
+    });
+  
+
+  this.service.getWeatherData().subscribe(
+    (data)=>{this.weather = data.weather}
+  );
+  } 
+  
+  constructor(private service:StudentService){}
 }
